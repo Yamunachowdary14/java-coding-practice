d5933713fcb8c870bfd4c60c64ccf25463305637
Yamuna Kambhapati <yamunachowdary245@gmail.com>
2025-02-26 22:49:33 +0530

    Initial commit with Java practice files

---
+++ datatype.java
@@ -0,0 +1,17 @@
+public class datatype {
+    public static void main(String[] args){
+        /*int i =10;
+        long l=5678l; we cannot take directly long so we use l 
+        short s=234;
+        byte b=2;
+        float f =7.90f; we cannot directly take float in java so here we use f
+        double d =8.567;
+        char ch ='y';
+        boolean yc = true; */
+
+
+
+
+
+    }    
+}
---
+++ literal.java
@@ -0,0 +1,6 @@
+public class literal {
+    public static void main(String a[]){
+        int  number =0b101;
+        System.out.println (number);
+    }
+}
