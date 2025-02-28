9621bb2b07edbab2c93cccb6bd9c269662041bf3
Yamuna Kambhapati <yamunachowdary245@gmail.com>
2025-02-28 11:07:41 +0530

    assignment.java

---
+++ assignment.java
@@ -0,0 +1,15 @@
+public class assignment {
+    public static void main(String[] args) {
+        int u =10;
+        // //add sign
+         System.out.println(u+=5);
+         System.out.println(u-=5);
+         System.out.println(u*=5);
+         System.out.println(u/=5);
+         System.out.println(u%=5);
+         System.out.println(u=5);
+         
+        
+    }
+    
+}
