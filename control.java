d40242379c75743006506b8c0f1d67001051d1f6
Yamuna Kambhapati <yamunachowdary245@gmail.com>
2025-03-05 20:33:24 +0530

    control.java

---
+++ control.java
@@ -0,0 +1,14 @@
+public class control{
+    public static void main(String[] args) {
+        int x=2;
+        if(x>10)
+            System.out.println(true);
+
+        else
+        System.out.println(false);
+
+
+
+        
+    }
+}
