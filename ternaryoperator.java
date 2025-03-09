6b23b5edcffe7354cea38540718eb629cc1bd869
Yamuna Kambhapati <yamunachowdary245@gmail.com>
2025-03-09 21:58:41 +0530

    Added switchstatment.java and updated ternaryoperator.java

--- ternaryoperator.java
+++ ternaryoperator.java
@@ -7,7 +7,7 @@ public class ternaryoperator {
         // else
         //     System.out.println("odd");
 
-        int  result = 0 ;
+        int  result = 0;
         result = x%2==0 ? 10: 20 ;
         System.out.println(result);
 
